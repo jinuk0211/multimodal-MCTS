de21acb706fbcf86ce6a8f7c20cc404ca62043c9
jinuklee <150532431+jinuk0211@users.noreply.github.com>
2025-01-30 15:31:10 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,88 @@
+import re
+from functools import partial
+import re
+from typing import Optional
+import sympy
+from pylatexenc import latex2text
+from sympy.parsing import sympy_parser
+#llm_verify -> extract_solution -> grade_answer함수 요약
+#------------------------------------
+from get_proposal import *
+def llm_verify(ans, real_ans, judge_model='gpt-4-1106-preview'):
+    prompt = '下面将输入两段文字，第一段文字为某道理科题目的一个解答或答案（不一定正确），第二段是这道题目的标准答案。请判断第一段解答得到的答案与标准答案在数学意义上是否一致，并根据判断直接输出‘0’或’1‘，不需要输出任何别的信息。如果答案一致，请输出‘1’；否则，只要答案不匹配，或者第一个文段中没有明确指出答案也没有输出latex表达式，请输出‘0’；如果第一段解答与标准答案之间关系模糊，请输出‘0’。\n'
+    qry = prompt + '文段1:' + ans + '\n' + '文段2:' + real_ans + '\n输出:'
+    lbl = ''
+    cnt = 5
+    while lbl == '' and cnt:
+        out = ''
+        try:
+            chat_comp = openai.ChatCompletion.create(model=judge_model, messages=[{"role": "user", "content": qry}])
+            out = chat_comp.choices[0].message.content[0]
+        except Exception as e:
+            print(f'Error:{e}\n')
+        if out == '0' or out == '1':
+            lbl = out
+        else:
+            cnt -= 1
+    if not cnt:
+        return 0
+    return int(lbl)
+
+def extract_summary_from_solution(solution: str):
+    pattern = r"\\boxed\{(.*)\}"
+    match = re.findall(pattern, solution)
+    if match:
+        summary = 'The final answer is ' + match[-1]
+    elif '####' in solution:
+        extracted = solution.split('####')[-1].strip()
+        if len(extracted) > 1:
+            if extracted[-1] == '.':
+                extracted = extracted[:-1].strip()
+            if len(extracted) > 1:
+                if extracted[0] == ':':
+                    extracted = extracted[1:].strip()
+        summary = 'The final answer is ' + extracted
+    elif 'The final answer is' in solution:
+        extracted = solution.split('The final answer is')[-1].strip()
+        if len(extracted) > 1:
+            if extracted[-1] == '.':
+                extracted = extracted[:-1].strip()
+            if len(extracted) > 1:
+                if extracted[0] == ':':
+                    extracted = extracted[1:].strip()
+        summary = 'The final answer is ' + extracted
+    elif 'The answer is' in solution:
+        extracted = solution.split('The answer is')[-1].strip()
+        if len(extracted) > 1:
+            if extracted[-1] == '.':
+                extracted = extracted[:-1].strip()
+            if len(extracted) > 1:
+                if extracted[0] == ':':
+                    extracted = extracted[1:].strip()
+        summary = 'The final answer is ' + extracted
+    elif 'final answer is' in solution:
+        extracted = solution.split('final answer is')[-1].strip()
+        if len(extracted) > 1:
+            if extracted[-1] == '.':
+                extracted = extracted[:-1].strip()
+            if len(extracted) > 1:
+                if extracted[0] == ':':
+                    extracted = extracted[1:].strip()
+        summary = 'The final answer is ' + extracted
+    elif 'answer is' in solution:
+        extracted = solution.split('answer is')[-1].strip()
+        if len(extracted) > 1:
+            if extracted[-1] == '.':
+                extracted = extracted[:-1].strip()
+            if len(extracted) > 1:
+                if extracted[0] == ':':
+                    extracted = extracted[1:].strip()
+        summary = 'The final answer is ' + extracted
+    else:
+        summary = ''
+    print('Extracted summary: ', summary, '\n')
+    return summary
+    
 def exact_match_score(prediction, ground_truth):
     prediction = extract_answer(prediction)
     return grade_answer(prediction, ground_truth)
